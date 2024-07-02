95c83d77b371c584578c4e47c910e95ce0814596
Dominik Bartoszewski <dominbartoszewski@gmail.com>
2024-07-02 13:31:11 +0200

    more tests

--- api.py
+++ api.py
@@ -1,6 +1,25 @@
 import requests
 import unittest
 
-r = requests.get('https://poetrydb.org/author,linecount/Shakespeare;14/lines')
-print(r.status_code)
+class TestClient(unittest.TestCase):
+    def setUp(self):
+        self.url = "https://poetrydb.org"
 
+    def test_returns_200(self):
+        result = requests.get('https://poetrydb.org/author')
+        self.assertEqual(result.status_code, 200)
+
+    def test_check_dictionary(self):
+        result = requests.get('https://poetrydb.org/author').json()
+        assert isinstance(result, dict), "Response data is not a dictionary"
+
+    def test_check_response_is_not_empty(self):
+        result = requests.get('https://poetrydb.org/author/William%20Shakespeare/author,title').json()
+        assert len(result) > 0, "'result' dict is empty"
+
+    def test_check_response_key(self):
+        result = requests.get('https://poetrydb.org/author').json()
+        assert "authors" in result, "Expected 'authors' key not found in the JSON response"
+
+if __name__ == '__main__':
+    unittest.main()
